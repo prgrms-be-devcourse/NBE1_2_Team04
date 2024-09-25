3aa3242b24d8013bb23b7a83f9b57910fad3195d
NaMinhyeok <nmh9097@gmail.com>
2024-09-25 12:42:38 +0900

    chore: #1 프로젝트 기본 세팅

---
+++ src/test/java/team4/footwithme/FootwithmeApplicationTest.java
@@ -0,0 +1,13 @@
+package team4.footwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootwithmeApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+  
+}
