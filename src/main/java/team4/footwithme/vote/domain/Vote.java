dd5b872a948c63be0e70e578ad9c46d340660b2b
NaMinhyeok <nmh9097@gmail.com>
2024-09-25 21:16:38 +0900

    feat: #2 투표 엔티티 수정
    - 투표의 상세항목을 저장할 테이블이 필요함
    - 투표의 상세항목을 투표 할 시 정보를 관리할 테이블이 필요함
    - 투표와 투표 상세항목은 라이프사이클이 완전히 같기 때문에 양방향연관관계 사용
    - 동일하게 싱글테이블 전략 사용
    - 투표 상세항목 VoteItem과 사용자의 투표를 기록하는 Choice 테이블은 FK로 연관관계 사용하지 않고 구현

--- src/main/java/team4/footwithme/vote/domain/Vote.java
+++ src/main/java/team4/footwithme/vote/domain/Vote.java
@@ -1,17 +1,17 @@
 package team4.footwithme.vote.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import team4.footwithme.global.domain.BaseEntity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,9 +24,39 @@ public class Vote extends BaseEntity {
     private Long voteId;
 
     @NotNull
+    private Long memberId;
+
+    @NotNull
+    private Long teamId;
+
+    @NotNull
     private String title;
 
     @NotNull
     private LocalDateTime endAt;
+
+    @OneToMany(mappedBy = "vote")
+    private List<VoteItem> voteItems = new ArrayList<>();
+
+    @Builder
+    private Vote(Long memberId, Long teamId, String title, LocalDateTime endAt) {
+        this.memberId = memberId;
+        this.teamId = teamId;
+        this.title = title;
+        this.endAt = endAt;
+    }
+
+    public static Vote create(Long memberId, Long teamId, String title, LocalDateTime endAt) {
+        return Vote.builder()
+            .memberId(memberId)
+            .teamId(teamId)
+            .title(title)
+            .endAt(endAt)
+            .build();
+    }
+
+    public void addChoice(VoteItem voteItem) {
+        this.voteItems.add(voteItem);
+    }
 
 }
