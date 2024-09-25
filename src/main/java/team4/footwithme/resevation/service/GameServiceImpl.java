ee6e7bc7dd78d6954314ea894e074f4b30fd496f
HMWG <nurung22222@gmail.com>
2024-09-25 17:15:40 +0900

    refactor: #3 Match -> Game 으로 이름 변경

--- src/main/java/team4/footwithme/resevation/service/GameServiceImpl.java
+++ src/main/java/team4/footwithme/resevation/service/GameServiceImpl.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MatchServiceImpl implements MatchService{
+public class GameServiceImpl implements GameService {
 }
