db597ce91a13c26f314e2090cc907188287ed16c
HMWG <nurung22222@gmail.com>
2024-09-26 10:41:33 +0900

    feat: #3 ChatMember 빌더 및 정적 팩토리 메소드 생성

--- src/main/java/team4/footwithme/chat/domain/ChatMember.java
+++ src/main/java/team4/footwithme/chat/domain/ChatMember.java
@@ -2,6 +2,7 @@ package team4.footwithme.chat.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team4.footwithme.global.domain.BaseEntity;
@@ -26,4 +27,16 @@ public class ChatMember extends BaseEntity {
     @JoinColumn(name = "chatroom_id", nullable = false)
     private Chatroom chatRoom;
 
+    @Builder
+    private ChatMember(Member member, Chatroom chatRoom) {
+        this.member = member;
+        this.chatRoom = chatRoom;
+    }
+
+    public static ChatMember create(Member member, Chatroom chatRoom) {
+        return ChatMember.builder()
+                .member(member)
+                .chatRoom(chatRoom)
+                .build();
+    }
 }
