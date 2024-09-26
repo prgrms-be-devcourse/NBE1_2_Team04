877482d3624321bf3bd6dab2327dec78996afc51
MoonJaeGyeong <bblbblan69@gmail.com>
2024-09-26 11:42:25 +0900

    feat : #6 Email로 Memeber 조회 기능 추가

--- src/main/java/team4/footwithme/member/repository/MemberRepository.java
+++ src/main/java/team4/footwithme/member/repository/MemberRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team4.footwithme.member.domain.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
