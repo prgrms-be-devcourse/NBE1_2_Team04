896b1cc44b538829e3cc2aaff9f5b907eef20bb8
MoonJaeGyeong <bblbblan69@gmail.com>
2024-09-26 15:34:01 +0900

    test : #6 memberRepository 테스트 작성

---
+++ src/test/java/team4/footwithme/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package team4.footwithme.member.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import team4.footwithme.member.domain.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("이메일로 멤버 조회")
+    void findByEmail() {
+        //given
+        String testEmail = "test@naver.com";
+        Member testMember = Member.create(testEmail, "password", "Test user", "010-1234-1234", LoginProvider.ORIGINAL, "test", Gender.MALE, MemberRole.USER, TermsAgreed.AGREE);
+        memberRepository.save(testMember);
+
+        //when
+        Member member = memberRepository.findByEmail(testEmail).get();
+
+        //then
+        assertThat(member.getEmail()).isEqualTo(testEmail);
+
+    }
+}
