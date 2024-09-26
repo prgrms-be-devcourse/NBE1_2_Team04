d53e4f5d11ff37bb1d431082ec9d3f28b0a4dd3e
MoonJaeGyeong <bblbblan69@gmail.com>
2024-09-26 14:35:02 +0900

    test : #6 JwtUtil 테스트 코드 작성

---
+++ src/test/java/team4/footwithme/member/jwt/JwtTokenUtilTest.java
@@ -0,0 +1,85 @@
+package team4.footwithme.member.jwt;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import team4.footwithme.member.domain.*;
+import team4.footwithme.member.jwt.response.TokenResponse;
+import team4.footwithme.member.repository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JwtTokenUtilTest {
+
+    @Autowired
+    private  MemberRepository memberRepository;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    private Member testMember;
+
+    @BeforeEach
+    void setUp(){
+        testMember = Member.builder()
+                .email("test@naver.com")
+                .password("password")
+                .name("Test User")
+                .phoneNumber("010-1234-5678")
+                .loginType(LoginType.builder()
+                        .loginProvider(LoginProvider.ORIGINAL)
+                        .snsId("example@naver.com")
+                        .build())
+                .gender(Gender.MALE)
+                .memberRole(MemberRole.USER)
+                .termsAgreed(TermsAgreed.AGREE)
+                .build();
+
+        memberRepository.save(testMember);
+    }
+
+    // 고민인 점 @SQLDelete 를 사용하게 되면 해당 테스트가 오류가 나서 일단 @SQLDelete를 주석 처리 했는데 어떤 방식으로 가능할까요?
+    @AfterEach
+    void afterDelete(){
+        memberRepository.delete(testMember);
+    }
+
+    @DisplayName("토큰 생성 테스트")
+    @Test
+    void createToken() {
+        String email = "test@naver.com";
+        TokenResponse tokenResponse = jwtTokenUtil.createToken(email);
+
+        assertThat(tokenResponse).isNotNull();
+    }
+
+    @DisplayName("토큰 추출 테스트")
+    @Test
+    void getEmailFromToken() {
+        String email = "test@naver.com";
+        TokenResponse tokenResponse = jwtTokenUtil.createToken(email);
+
+        assertThat(tokenResponse).isNotNull();
+        String emailFromAccessToken = jwtTokenUtil.getEmailFromToken(tokenResponse.accessToken());
+        String emailFromRefreshToken = jwtTokenUtil.getEmailFromToken(tokenResponse.refreshToken());
+
+        assertThat(emailFromAccessToken).isEqualTo(email);
+        assertThat(emailFromRefreshToken).isEqualTo(email);
+    }
+
+    @DisplayName("RefreshToken 으로 AccessToken 생성")
+    @Test
+    void reCreateAccessTokenByRefreshToken() {
+        String email = "test@naver.com";
+        TokenResponse tokenResponse = jwtTokenUtil.createToken(email);
+
+        String reCreateAccessToken = jwtTokenUtil.reCreateAccessToken(tokenResponse.refreshToken());
+
+        assertThat(reCreateAccessToken).isNotNull();
+    }
+
+}
