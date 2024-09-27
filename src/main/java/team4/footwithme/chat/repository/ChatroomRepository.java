6daa167ad222d77dd0cb84cb00ac4de4721469fa
HMWG <nurung22222@gmail.com>
2024-09-27 13:35:37 +0900

    feat: #3 채팅방 생성 및 삭제 서비스 로직 작성
    - Redis pub/sub 서비스 구현
    - Redis 채팅방 토픽 서비스 구현
    - 채팅방 조회 쿼리 메소드 추가
    - 채팅방 생성 및 삭제 서비스 구현

--- src/main/java/team4/footwithme/chat/repository/ChatroomRepository.java
+++ src/main/java/team4/footwithme/chat/repository/ChatroomRepository.java
@@ -1,9 +1,16 @@
 package team4.footwithme.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team4.footwithme.chat.domain.Chatroom;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
+
+    @Query("select c from Chatroom c where c.isDeleted = 'false' and c.chatroomId = :id")
+    Optional<Chatroom> findByChatroomId(@Param("id") Long chatroomId);
 }
---
+++ src/main/java/team4/footwithme/chat/service/RedisPublisher.java
@@ -0,0 +1,17 @@
+package team4.footwithme.chat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+import team4.footwithme.chat.domain.Chat;
+
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisChatTemplate;
+
+    public void publish(ChannelTopic topic, Chat chat) {
+        redisChatTemplate.convertAndSend(topic.getTopic(), chat);
+    }
+}
