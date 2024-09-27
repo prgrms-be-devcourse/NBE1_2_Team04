87ae89c82c06a9be17d70bb7509823f8fe385d92
NaMinhyeok <nmh9097@gmail.com>
2024-09-27 16:23:03 +0900

    fix: #9 내장 레디스 맥 설정

--- src/main/java/team4/footwithme/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/team4/footwithme/config/redis/EmbeddedRedisConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
+import java.io.File;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 
 /**
  * 로컬 환경일경우 내장 레디스가 실행됩니다.
@@ -22,8 +26,12 @@ public class EmbeddedRedisConfig {
 
     @PostConstruct
     public void redisServer() {
-        redisServer = new RedisServer(redisPort);
-        redisServer.start();
+        if(isArmMac()) {
+            redisServer = new RedisServer(getRedisServerExecutable(), redisPort);
+        } else {
+            redisServer = new RedisServer(redisPort);
+            redisServer.start();
+        }
     }
 
     @PreDestroy
@@ -31,5 +39,17 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    private File getRedisServerExecutable(){
+        try {
+            return new File("src/main/resources/binary/redis/redis-server-6.2.5-mac-arm64");
+        } catch (Exception e) {
+            throw new NoSuchElementException("Redis Server Executable File Not Found");
+        }
+    }
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64")
+            && Objects.equals(System.getProperty("os.name"), "Mac OS X");
     }
 }
