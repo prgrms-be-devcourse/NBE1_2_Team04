3ca5a99ff53d1eb298298f4244f444c92e4c5fb4
MoonJaeGyeong <bblbblan69@gmail.com>
2024-09-27 16:08:44 +0900

    feat : #13 쿼리문 추가

--- src/main/java/team4/footwithme/member/repository/MemberRepository.java
+++ src/main/java/team4/footwithme/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package team4.footwithme.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import team4.footwithme.member.domain.Member;
 
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
+
+    @Query("select m from Member m where m.isDeleted = 'false' and m.email = :email")
     Optional<Member> findByEmail(String email);
 }
