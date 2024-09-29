345e5acc74bb6bc14ca7e3cdebf0840b1454efa4
HMWG <nurung22222@gmail.com>
2024-09-29 21:34:47 +0900

    feat: #25 서비스에 메소드 정의
    - ChatMemberService
    - ChatroomService
    - ChatService

--- src/main/java/team4/footwithme/chat/service/ChatService.java
+++ src/main/java/team4/footwithme/chat/service/ChatService.java
@@ -1,4 +1,8 @@
 package team4.footwithme.chat.service;
 
+import team4.footwithme.chat.service.request.ChatServiceRequest;
+
 public interface ChatService {
+
+    void sendMessage(ChatServiceRequest request, String email);
 }
