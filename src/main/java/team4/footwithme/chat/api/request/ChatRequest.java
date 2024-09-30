21e9bfed1800b7432ec321c45aec23f20271822e
HMWG <nurung22222@gmail.com>
2024-10-01 01:18:34 +0900

    fix: #36 validation 수정
    - 아이디 검증 @NotBlank -> @NotNull
    - 메세지 검증 @NotBlank -> @NotEmpty

--- src/main/java/team4/footwithme/chat/api/request/ChatRequest.java
+++ src/main/java/team4/footwithme/chat/api/request/ChatRequest.java
@@ -1,12 +1,13 @@
 package team4.footwithme.chat.api.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import team4.footwithme.chat.service.request.ChatServiceRequest;
 
 public record ChatRequest(
-        @NotBlank(message = "채팅방 아이디는 필수입니다.")
+        @NotNull(message = "채팅방 아이디는 필수입니다.")
         Long chatroomId,
-        @NotBlank(message = "채팅 메세지는 필수입니다.")
+        @NotEmpty(message = "채팅 메세지는 필수입니다.")
         String message
 ) {
     public ChatServiceRequest toServiceRequest() {
