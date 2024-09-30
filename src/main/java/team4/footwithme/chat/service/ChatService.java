5d27647604794daacf017f744b66a8cdc4da2d26
HMWG <nurung22222@gmail.com>
2024-09-30 12:03:28 +0900

    feat: #25 채팅 조회 서비스 구현
    - 채팅 조회 페이지네이션 QueryDSL 구성
    - 채팅 및 채팅방 조회 리포지토리 작성
    - 채팅 조회 서비스로직 작성
    - 채팅 검증시 메소드에서 예외를 던지게 변경

--- src/main/java/team4/footwithme/chat/service/ChatService.java
+++ src/main/java/team4/footwithme/chat/service/ChatService.java
@@ -1,8 +1,13 @@
 package team4.footwithme.chat.service;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import team4.footwithme.chat.service.request.ChatServiceRequest;
+import team4.footwithme.chat.service.response.ChatResponse;
 
 public interface ChatService {
 
     void sendMessage(ChatServiceRequest request, String email);
+
+    Slice<ChatResponse> getChatList(Long chatroomId, PageRequest pageRequest, String email);
 }
--- src/main/java/team4/footwithme/chat/service/ChatServiceImpl.java
+++ src/main/java/team4/footwithme/chat/service/ChatServiceImpl.java
@@ -1,6 +1,8 @@
 package team4.footwithme.chat.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,7 @@ import team4.footwithme.chat.repository.ChatRepository;
 import team4.footwithme.chat.repository.ChatroomRepository;
 import team4.footwithme.chat.repository.RedisChatroomRepository;
 import team4.footwithme.chat.service.request.ChatServiceRequest;
+import team4.footwithme.chat.service.response.ChatResponse;
 import team4.footwithme.member.domain.Member;
 import team4.footwithme.member.repository.MemberRepository;
 
@@ -38,9 +41,7 @@ public class ChatServiceImpl implements ChatService{
         Chatroom chatroom = chatroomRepository.findByChatroomId(request.ChatroomId()).orElseThrow(()-> new IllegalArgumentException("Chatroom not found"));
 
         // 채팅방에 참여한 멤버인지 검증
-        if(!checkMember(member, chatroom)) {
-            throw new IllegalArgumentException("Invalid member");
-        }
+        checkMember(member, chatroom);
 
         Chat chat = Chat.create(chatroom, member, request.message());
 
@@ -59,11 +60,22 @@ public class ChatServiceImpl implements ChatService{
      * 페이지네이션
      * 채팅방 메세지를 보려면 채팅방에 소속된 멤버여야 함
      */
+    @Transactional(readOnly = true)
+    public Slice<ChatResponse> getChatList(Long chatroomId, PageRequest pageRequest, String email) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(()-> new IllegalArgumentException("Member not found"));
+        Chatroom chatroom = chatroomRepository.findByChatroomId(chatroomId).orElseThrow(()-> new IllegalArgumentException("Chatroom not found"));
+
+        checkMember(member, chatroom);
+
+        return chatRepository.findChatByChatroom(chatroom, pageRequest);
+    }
 
     /**
      * 채팅방에 소속된 멤버인지 검증하는 메소드
      */
-    public boolean checkMember(Member member, Chatroom chatroom) {
-        return chatMemberRepository.existsByMemberAndChatRoom(member, chatroom);
+    public void checkMember(Member member, Chatroom chatroom) {
+        if(!chatMemberRepository.existsByMemberAndChatRoom(member, chatroom)) {
+            throw new IllegalArgumentException("Invalid member");
+        }
     }
 }
