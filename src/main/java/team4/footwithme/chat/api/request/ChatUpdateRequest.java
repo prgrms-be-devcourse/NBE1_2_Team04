9651811276f37380341b2a34791c866bd410ba18
HMWG <nurung22222@gmail.com>
2024-10-01 11:47:47 +0900

    feat: #36 채팅 validation 수정

--- src/main/java/team4/footwithme/chat/api/request/ChatUpdateRequest.java
+++ src/main/java/team4/footwithme/chat/api/request/ChatUpdateRequest.java
@@ -1,10 +1,10 @@
 package team4.footwithme.chat.api.request;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import team4.footwithme.chat.service.request.ChatServiceRequest;
 
 public record ChatUpdateRequest(
-        @NotEmpty(message = "채팅 메세지는 필수입니다.")
+        @NotBlank(message = "채팅 메세지는 필수입니다.")
         String message
 ) {
     // 코드 재사용을 원해서 serviceRequest에 null값을 넣어줬는데 괜찮은지 모르겠네요 ㅜㅜ
