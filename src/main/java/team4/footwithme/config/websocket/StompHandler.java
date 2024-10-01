9c20e0111163f3e633fd733889d5c8d297d7c8ee
HMWG <nurung22222@gmail.com>
2024-10-01 10:49:16 +0900

    feat: #36 웹소켓 접속시 인증 인터셉터 구현

---
+++ src/main/java/team4/footwithme/config/websocket/StompHandler.java
@@ -0,0 +1,33 @@
+package team4.footwithme.config.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+import team4.footwithme.member.jwt.JwtTokenUtil;
+
+@Component
+@RequiredArgsConstructor
+public class StompHandler implements ChannelInterceptor {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    // websocket을 통해 들어온 요청이 처리 되기 전 실행
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        // websocket 연결시 헤더의 jwt token 검증
+        if(StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String token = accessor.getFirstNativeHeader("Authorization");
+            if (token==null) {
+                throw new IllegalArgumentException("Authorization header missing");
+            }
+            token = token.substring(7).trim();
+            jwtTokenUtil.tokenValidation(token);
+        }
+        return message;
+    }
+}
--- src/main/java/team4/footwithme/config/websocket/WebSocketConfig.java
+++ src/main/java/team4/footwithme/config/websocket/WebSocketConfig.java
@@ -2,6 +2,7 @@ package team4.footwithme.config.websocket;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,6 +12,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    // 웹소켓 연결되기 전 인터셉트
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
 
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
