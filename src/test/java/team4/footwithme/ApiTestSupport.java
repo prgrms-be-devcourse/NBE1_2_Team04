efb42b817fe906087334881cdec9c5904664926b
HMWG <nurung22222@gmail.com>
2024-10-05 02:17:29 +0900

    test: #37 채팅 관련 API 테스트 코드 작성

--- src/test/java/team4/footwithme/ApiTestSupport.java
+++ src/test/java/team4/footwithme/ApiTestSupport.java
@@ -2,16 +2,27 @@ package team4.footwithme;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import team4.footwithme.chat.api.ChatApi;
+import team4.footwithme.chat.api.ChatMemberApi;
+import team4.footwithme.chat.api.ChatroomApi;
+import team4.footwithme.chat.service.ChatMemberService;
+import team4.footwithme.chat.service.ChatService;
+import team4.footwithme.chat.service.ChatroomService;
+import team4.footwithme.config.SecurityConfig;
 import team4.footwithme.member.jwt.JwtTokenFilter;
 import team4.footwithme.vote.api.VoteApi;
 import team4.footwithme.vote.service.VoteService;
 
 @WebMvcTest(controllers = {
-    VoteApi.class
+    VoteApi.class, ChatApi.class, ChatroomApi.class, ChatMemberApi.class,
 })
+@AutoConfigureMockMvc(addFilters = false)
+@Import(SecurityConfig.class)
 public abstract class ApiTestSupport {
 
     @Autowired
@@ -22,6 +33,15 @@ public abstract class ApiTestSupport {
 
     @MockBean
     protected VoteService voteService;
+
+    @MockBean
+    protected ChatService chatService;
+
+    @MockBean
+    protected ChatroomService chatroomService;
+
+    @MockBean
+    protected ChatMemberService chatMemberService;
 
     @MockBean
     protected JwtTokenFilter jwtTokenFilter;
