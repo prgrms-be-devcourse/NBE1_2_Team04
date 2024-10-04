ae1ad89cb55877654a238ea5652e9bf20118ca66
HMWG <nurung22222@gmail.com>
2024-10-05 02:16:44 +0900

    test: #37 테스트 환경에서 @AuthenticationPrincipal을 통해 PrincipalDetails를 받아오는 커스텀 어노테이션 추가

---
+++ src/test/java/team4/footwithme/security/CustomSecurityContextFactory.java
@@ -0,0 +1,29 @@
+package team4.footwithme.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import team4.footwithme.member.domain.Gender;
+import team4.footwithme.member.domain.Member;
+import team4.footwithme.member.domain.TermsAgreed;
+import team4.footwithme.member.jwt.PrincipalDetails;
+
+public class CustomSecurityContextFactory implements WithSecurityContextFactory<WithMockPrincipalDetail> {
+
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockPrincipalDetail annotation) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        PrincipalDetails principal = new PrincipalDetails();
+        Member member = Member.create(annotation.email(), annotation.password(), "이름 1", "010-1234-5678", null, null, Gender.FEMALE, annotation.role(), TermsAgreed.AGREE);
+
+        principal.setMember(member);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
+        context.setAuthentication(auth);
+        return context;
+    }
+}
