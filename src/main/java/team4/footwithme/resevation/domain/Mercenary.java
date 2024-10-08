317b857ce36521c3b39070ac24f9f9bd715079a6
MoonJaeGyeong <bblbblan69@gmail.com>
2024-10-08 15:34:15 +0900

    fix: #73 삭제 기능 오류 수정

--- src/main/java/team4/footwithme/resevation/domain/Mercenary.java
+++ src/main/java/team4/footwithme/resevation/domain/Mercenary.java
@@ -10,7 +10,7 @@ import team4.footwithme.global.domain.BaseEntity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE mercenary SET is_deleted = TRUE WHERE mercenary_id = ?")
+@SQLDelete(sql = "UPDATE mercenary SET is_deleted = 'TRUE' WHERE mercenary_id = ?")
 @Entity
 public class Mercenary extends BaseEntity {
 
@@ -25,16 +25,22 @@ public class Mercenary extends BaseEntity {
     @Column(length = 200, nullable = true)
     private String description;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
+
     @Builder
-    private Mercenary(Participant participant, String description) {
+    private Mercenary(Participant participant, String description, Reservation reservation) {
         this.participant = participant;
         this.description = description;
+        this.reservation = reservation;
     }
 
-    public static Mercenary create(Participant participant, String description) {
+    public static Mercenary create(Participant participant, String description, Reservation reservation) {
         return Mercenary.builder()
             .participant(participant)
             .description(description)
+                .reservation(reservation)
             .build();
     }
 
