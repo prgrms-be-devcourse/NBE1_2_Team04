310aa0de7ede907b31c9e6f3d8fdb42d35b8f09a
MoonJaeGyeong <bblbblan69@gmail.com>
2024-10-08 22:25:49 +0900

    test : #66 member API 테스트 추가

--- src/test/java/team4/footwithme/ApiTestSupport.java
+++ src/test/java/team4/footwithme/ApiTestSupport.java
@@ -12,7 +12,10 @@ import team4.footwithme.chat.api.ChatroomApi;
 import team4.footwithme.chat.service.ChatMemberService;
 import team4.footwithme.chat.service.ChatService;
 import team4.footwithme.chat.service.ChatroomService;
+import team4.footwithme.member.api.MemberApi;
 import team4.footwithme.member.jwt.JwtTokenFilter;
+import team4.footwithme.member.service.CookieService;
+import team4.footwithme.member.service.MemberService;
 import team4.footwithme.stadium.api.CourtMerchantApi;
 import team4.footwithme.stadium.api.StadiumMerchantApi;
 import team4.footwithme.stadium.service.CourtService;
@@ -21,7 +24,7 @@ import team4.footwithme.vote.api.VoteApi;
 import team4.footwithme.vote.service.VoteService;
 
 @WebMvcTest(controllers = {
-    VoteApi.class, ChatApi.class, ChatroomApi.class, ChatMemberApi.class, StadiumMerchantApi.class, CourtMerchantApi.class
+    VoteApi.class, ChatApi.class, ChatroomApi.class, ChatMemberApi.class, StadiumMerchantApi.class, CourtMerchantApi.class, MemberApi.class
 })
 @AutoConfigureMockMvc(addFilters = false)
 public abstract class ApiTestSupport {
@@ -52,6 +55,12 @@ public abstract class ApiTestSupport {
 
     @MockBean
     protected StadiumService stadiumService;
+
+    @MockBean
+    protected MemberService memberService;
+
+    @MockBean
+    protected CookieService cookieService;
 
     // MockBean 통해서 실제 빈을 대체하는 가짜 빈을 주입
     // 사용하는 서비스들은 모두 MockBean으로 주입
