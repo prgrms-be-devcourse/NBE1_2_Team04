47626996f53b9a80313805c91985a19b6272fc25
NaMinhyeok <nmh9097@gmail.com>
2024-10-08 09:52:53 +0900

    test: #69 테스트 시 시큐리티문제 해결

--- src/test/java/team4/footwithme/vote/api/VoteApiTest.java
+++ src/test/java/team4/footwithme/vote/api/VoteApiTest.java
@@ -9,6 +9,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import team4.footwithme.ApiTestSupport;
 import team4.footwithme.config.SecurityConfig;
 import team4.footwithme.member.domain.Member;
+import team4.footwithme.security.WithMockPrincipalDetail;
 import team4.footwithme.vote.api.request.CourtChoices;
 import team4.footwithme.vote.api.request.VoteCourtCreateRequest;
 import team4.footwithme.vote.service.request.VoteCourtCreateServiceRequest;
@@ -28,11 +29,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Import(SecurityConfig.class)
 class VoteApiTest extends ApiTestSupport {
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성한다.")
+    @WithMockPrincipalDetail(email = "test@gmail.com")
     @Test
     void createLocateVote() throws Exception {
         //given
@@ -47,12 +47,12 @@ class VoteApiTest extends ApiTestSupport {
             "연말 행사 투표",
             endAt,
             "진행 중",
-            List.of(new VoteItemResponse(1L, "최강 풋살장", List.of()),
-                new VoteItemResponse(2L, "열정 풋살장", List.of())
+            List.of(new VoteItemResponse(1L, "최강 풋살장", List.of(1L)),
+                new VoteItemResponse(2L, "열정 풋살장", List.of(1L))
             )
         );
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(response);
 
         mockMvc.perform(post("/api/v1/votes/stadiums/{teamId}", 1L)
@@ -71,16 +71,14 @@ class VoteApiTest extends ApiTestSupport {
             .andExpect(jsonPath("$.data.choices").isArray())
             .andExpect(jsonPath("$.data.choices[0].voteItemId").value(1L))
             .andExpect(jsonPath("$.data.choices[0].content").value("최강 풋살장"))
-            .andExpect(jsonPath("$.data.choices[0].voteCount").value(0L))
+            .andExpect(jsonPath("$.data.choices[0].memberIds[0]").value(1L))
             .andExpect(jsonPath("$.data.choices[1].voteItemId").value(2L))
             .andExpect(jsonPath("$.data.choices[1].content").value("열정 풋살장"))
-            .andExpect(jsonPath("$.data.choices[1].voteCount").value(0L));
+            .andExpect(jsonPath("$.data.choices[1].memberIds[0]").value(1L));
 
     }
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성 할 때 제목은 필수이다.")
-    @WithMockUser
     @Test
     void createLocateVoteWhenTitleIsNotExistThenThrowException() throws Exception {
         //given
@@ -89,7 +87,7 @@ class VoteApiTest extends ApiTestSupport {
         CourtChoices courtChoices2 = new CourtChoices(2L);
         VoteCourtCreateRequest request = new VoteCourtCreateRequest(null, endAt, List.of(courtChoices1, courtChoices2));
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(new VoteResponse(
                 1L,
                 "연말 행사 투표",
@@ -112,7 +110,6 @@ class VoteApiTest extends ApiTestSupport {
 
     }
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성 할 때 제목은 50자 이하이다.")
     @WithMockUser
     @Test
@@ -123,7 +120,7 @@ class VoteApiTest extends ApiTestSupport {
         CourtChoices courtChoices2 = new CourtChoices(2L);
         VoteCourtCreateRequest request = new VoteCourtCreateRequest("a".repeat(51), endAt, List.of(courtChoices1, courtChoices2));
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(new VoteResponse(
                 1L,
                 "연말 행사 투표",
@@ -146,9 +143,8 @@ class VoteApiTest extends ApiTestSupport {
 
     }
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성 할 때 시간은 현재 시간보다 미래의 시간으로 지정해야한다.")
-    @WithMockUser
+    @WithMockPrincipalDetail(email = "test@gmail.com")
     @Test
     void createLocateVoteWhenEndAtIsBeforeNowThenThrowException() throws Exception {
         //given
@@ -157,7 +153,7 @@ class VoteApiTest extends ApiTestSupport {
         CourtChoices courtChoices2 = new CourtChoices(2L);
         VoteCourtCreateRequest request = new VoteCourtCreateRequest("연말 행사 투표", endAt, List.of(courtChoices1, courtChoices2));
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(new VoteResponse(
                 1L,
                 "연말 행사 투표",
@@ -180,16 +176,15 @@ class VoteApiTest extends ApiTestSupport {
 
     }
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성 할 때 구장을 최소 하나 이상 선택해야한다.")
-    @WithMockUser
+    @WithMockPrincipalDetail(email = "test@gmail.com")
     @Test
     void createLocateVoteWhenStadiumIsNullThenThrowException() throws Exception {
         //given
         LocalDateTime endAt = LocalDateTime.now().plusDays(1);
         VoteCourtCreateRequest request = new VoteCourtCreateRequest("연말 행사 투표", endAt, List.of());
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(new VoteResponse(
                 1L,
                 "연말 행사 투표",
@@ -212,9 +207,7 @@ class VoteApiTest extends ApiTestSupport {
 
     }
 
-    @Disabled
     @DisplayName("새로운 구장 투표를 생성 할 때 구장을 중복된 구장을 선택 할 수 없다.")
-    @WithMockUser
     @Test
     void createLocateVoteWhenStadiumIsDuplicateThenThrowException() throws Exception {
         //given
@@ -223,7 +216,7 @@ class VoteApiTest extends ApiTestSupport {
         CourtChoices courtChoices2 = new CourtChoices(1L);
         VoteCourtCreateRequest request = new VoteCourtCreateRequest("연말 행사 투표", endAt, List.of(courtChoices1, courtChoices2));
 
-        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any(Member.class)))
+        given(voteService.createCourtVote(any(VoteCourtCreateServiceRequest.class), eq(1L), any()))
             .willReturn(new VoteResponse(
                 1L,
                 "연말 행사 투표",
