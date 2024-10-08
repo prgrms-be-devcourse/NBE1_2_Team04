03d4da13b2e6812fb6db464afa91d63f80344e2c
HMWG <nurung22222@gmail.com>
2024-10-08 11:43:26 +0900

    feat: #68 Mercenary 및 Participant 리포지토리 필요한 메소드 구현

---
+++ src/main/java/team4/footwithme/resevation/repository/MWParticipantRepository.java
@@ -0,0 +1,20 @@
+package team4.footwithme.resevation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import team4.footwithme.resevation.domain.Participant;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MWParticipantRepository extends JpaRepository<Participant,Long> , CustomParticipantRepository{
+    @Query("select p from Participant p where p.isDeleted = 'false' and p.reservation.reservationId = :id")
+    List<Participant> findParticipantsByReservationId(@Param("id") Long reservationId);
+
+    @Query("select p from Participant p where p.isDeleted = 'false' and p.reservation.reservationId = :rid and p.member.memberId = :mid")
+    Optional<Participant> findParticipantsByReservationIdAndMemberId(@Param("rid") Long reservationId, @Param("mid") Long memberId);
+
+    @Query("select p from Participant p where p.isDeleted = 'false' and p.participantId = :id")
+    Optional<Participant> findParticipantsByParticipantId(@Param("id") Long participantId);
+}
